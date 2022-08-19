a4cbcdaa6093c1c5d2d832f643cccc8c3366e0dc
Brendan Cronin <brendan.cronin@mercedes-benz.com>
2022-08-19 04:55:30 +0200

    Add draft of basic structure Signed-off-by: Brendan Cronin <brendan.cronin@mercedes-benz.com>

---
+++ edc-extensions/transferprocess-sftp/src/main/java/net/catenax/edc/transferprocess/sftp/SftpProvisionerImpl.java
@@ -0,0 +1,37 @@
+/*
+ *  Copyright (c) 2022 Mercedes-Benz Tech Innovation GmbH
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Mercedes-Benz Tech Innovation GmbH - Initial API and Implementation
+ *
+ */
+
+package net.catenax.edc.transferprocess.sftp;
+
+public class SftpProvisionerImpl implements SftpProvisioner {
+    @Override
+    public void createUser() {
+
+    }
+
+    @Override
+    public void deleteUser() {
+
+    }
+
+    @Override
+    public void createBucket() {
+
+    }
+
+    @Override
+    public void deleteBucket() {
+
+    }
+}
