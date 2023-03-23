05e6d083fd941f14efeeb2460954068b25c56959
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2023-03-23 15:08:43 +0100

    fixed deletion leftovers

--- edc-tests/e2e-tests/src/test/java/org/eclipse/tractusx/edc/tests/DataWiper.java
+++ edc-tests/e2e-tests/src/test/java/org/eclipse/tractusx/edc/tests/DataWiper.java
@@ -24,19 +24,17 @@ public class DataWiper {
 
     private void clearContractDefinitions() {
         var cds = context.getService(ContractDefinitionStore.class);
-        cds.findAll(QuerySpec.none())
-                .forEach(cd -> cds.deleteById(cd.getId()));
+        cds.findAll(QuerySpec.max()).forEach(cd -> cds.deleteById(cd.getId()));
     }
 
     private void clearPolicies() {
         var ps = context.getService(PolicyDefinitionStore.class);
-        ps.findAll(QuerySpec.none()).collect(Collectors.toList())
-                .forEach(p -> ps.deleteById(p.getId()));
+        // must .collect() here, otherwise we'll get a ConcurrentModificationException
+        ps.findAll(QuerySpec.max()).collect(Collectors.toList()).forEach(p -> ps.deleteById(p.getId()));
     }
 
     private void clearAssetIndex() {
         var index = context.getService(AssetIndex.class);
-        index.queryAssets(QuerySpec.none())
-                .forEach(asset -> index.deleteById(asset.getId()));
+        index.queryAssets(QuerySpec.max()).forEach(asset -> index.deleteById(asset.getId()));
     }
 }
