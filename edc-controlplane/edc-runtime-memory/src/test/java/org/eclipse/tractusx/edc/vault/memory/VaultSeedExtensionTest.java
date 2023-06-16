6d502943aa10502fbce905adb04008d951c9d878
Paul Latzelsperger <43503240+paullatzelsperger@users.noreply.github.com>
2023-06-16 13:23:22 +0200

    chore: remove in-mem vault (#498)

--- edc-controlplane/edc-runtime-memory/src/test/java/org/eclipse/tractusx/edc/vault/memory/VaultSeedExtensionTest.java
+++ edc-controlplane/edc-runtime-memory/src/test/java/org/eclipse/tractusx/edc/vault/memory/VaultSeedExtensionTest.java
@@ -14,10 +14,15 @@
 
 package org.eclipse.tractusx.edc.vault.memory;
 
+import org.eclipse.edc.connector.core.vault.InMemoryVault;
+import org.eclipse.edc.junit.extensions.DependencyInjectionExtension;
 import org.eclipse.edc.spi.monitor.Monitor;
+import org.eclipse.edc.spi.security.Vault;
 import org.eclipse.edc.spi.system.ServiceExtensionContext;
+import org.eclipse.edc.spi.system.injection.ObjectFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -25,32 +30,35 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.anyString;
 import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-class VaultMemoryExtensionTest {
-    private VaultMemoryExtension extension;
+@ExtendWith(DependencyInjectionExtension.class)
+class VaultSeedExtensionTest {
+    private VaultSeedExtension extension;
     private ServiceExtensionContext context;
     private Monitor monitor;
 
     @BeforeEach
-    void setup() {
-        extension = new VaultMemoryExtension();
-        context = mock(ServiceExtensionContext.class);
+    void setup(ServiceExtensionContext context, ObjectFactory factory) {
+        this.context = spy(context);
         monitor = mock(Monitor.class);
-        when(context.getMonitor()).thenReturn(monitor);
+        context.registerService(Monitor.class, monitor);
+        context.registerService(Vault.class, new InMemoryVault(monitor));
+        extension = factory.constructInstance(VaultSeedExtension.class);
     }
 
     @Test
     void name() {
-        assertThat(extension.name()).isEqualTo("In-Memory Vault Extension");
+        assertThat(extension.name()).isEqualTo("Vault Seed Extension");
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"key1:", "key1:value1", "key1:value1;", ";key1:value1", ";sdf;key1:value1"})
     void createInMemVault_validString(String secret) {
-        when(context.getSetting(eq(VaultMemoryExtension.VAULT_MEMORY_SECRETS_PROPERTY), eq(null))).thenReturn(secret);
+        when(context.getSetting(eq(VaultSeedExtension.VAULT_MEMORY_SECRETS_PROPERTY), eq(null))).thenReturn(secret);
         extension.createInMemVault(context);
         verify(monitor, times(1)).debug(anyString());
     }
