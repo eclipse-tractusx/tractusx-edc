dfb837dc3a2555b936c74378b3e850a5bd544d69
Paul Latzelsperger <43503240+paullatzelsperger@users.noreply.github.com>
2023-07-10 22:10:11 +0200

    feat(test): add integration tests for MIW (#585)
    * feat(test): add integration tests for MIW
    
    * delete unneeded resources

--- edc-tests/miw-tests/build.gradle.kts
+++ edc-tests/miw-tests/build.gradle.kts
@@ -44,7 +44,11 @@ dependencies {
     testCompileOnly(project(":edc-tests:runtime:runtime-memory"))
     testCompileOnly(project(":edc-tests:runtime:runtime-memory-ssi"))
     testCompileOnly(project(":edc-tests:runtime:runtime-postgresql"))
+    testImplementation(project(":edc-extensions:ssi:ssi-miw-credential-client"))
     testImplementation(libs.edc.auth.oauth2.client)
+
+    runtimeOnly(libs.tink)
+
 }
 
 // do not publish
