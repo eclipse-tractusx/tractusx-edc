32110f03911921d723a8ab0b0c1b5cb165c626d7
Enrico Risa <enrico.risa@gmail.com>
2023-11-09 16:34:53 +0100

    fix: backport the pool fix by copying the upstream module

---
+++ edc-extensions/sql-pool/src/main/java/org/eclipse/tractusx/edc/sql/pool/commons/CommonsConnectionPoolServiceExtension.java
@@ -0,0 +1,154 @@
+/*
+ *  Copyright (c) 2023 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Bayerische Motoren Werke Aktiengesellschaft (BMW AG) - initial API and implementation
+ *
+ */
+
+package org.eclipse.tractusx.edc.sql.pool.commons;
+
+import org.eclipse.edc.runtime.metamodel.annotation.Extension;
+import org.eclipse.edc.runtime.metamodel.annotation.Inject;
+import org.eclipse.edc.spi.monitor.Monitor;
+import org.eclipse.edc.spi.system.ServiceExtension;
+import org.eclipse.edc.spi.system.ServiceExtensionContext;
+import org.eclipse.edc.spi.system.configuration.Config;
+import org.eclipse.edc.sql.ConnectionFactory;
+import org.eclipse.edc.sql.datasource.ConnectionFactoryDataSource;
+import org.eclipse.edc.sql.datasource.ConnectionPoolDataSource;
+import org.eclipse.edc.transaction.datasource.spi.DataSourceRegistry;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Properties;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import javax.sql.DataSource;
+
+import static java.lang.String.format;
+import static org.eclipse.tractusx.edc.sql.pool.commons.CommonsConnectionPoolConfigKeys.CONFIGURATION_MAPPING;
+import static org.eclipse.tractusx.edc.sql.pool.commons.CommonsConnectionPoolConfigKeys.POOL_CONNECTIONS_MAX_IDLE;
+import static org.eclipse.tractusx.edc.sql.pool.commons.CommonsConnectionPoolConfigKeys.POOL_CONNECTIONS_MAX_TOTAL;
+import static org.eclipse.tractusx.edc.sql.pool.commons.CommonsConnectionPoolConfigKeys.POOL_CONNECTIONS_MIN_IDLE;
+import static org.eclipse.tractusx.edc.sql.pool.commons.CommonsConnectionPoolConfigKeys.POOL_CONNECTION_TEST_ON_BORROW;
+import static org.eclipse.tractusx.edc.sql.pool.commons.CommonsConnectionPoolConfigKeys.POOL_CONNECTION_TEST_ON_CREATE;
+import static org.eclipse.tractusx.edc.sql.pool.commons.CommonsConnectionPoolConfigKeys.POOL_CONNECTION_TEST_ON_RETURN;
+import static org.eclipse.tractusx.edc.sql.pool.commons.CommonsConnectionPoolConfigKeys.POOL_CONNECTION_TEST_QUERY;
+import static org.eclipse.tractusx.edc.sql.pool.commons.CommonsConnectionPoolConfigKeys.POOL_CONNECTION_TEST_WHILE_IDLE;
+import static org.eclipse.tractusx.edc.sql.pool.commons.CommonsConnectionPoolConfigKeys.URL;
+
+@Extension(value = CommonsConnectionPoolServiceExtension.NAME)
+public class CommonsConnectionPoolServiceExtension implements ServiceExtension {
+    public static final String NAME = "Commons Connection Pool";
+    public static final String EDC_DATASOURCE_PREFIX = "edc.datasource";
+    private final List<CommonsConnectionPool> commonsConnectionPools = new LinkedList<>();
+    @Inject
+    private DataSourceRegistry dataSourceRegistry;
+
+    @Inject
+    private Monitor monitor;
+
+    @Override
+    public String name() {
+        return NAME;
+    }
+
+    @Override
+    public void initialize(ServiceExtensionContext context) {
+        Config config = context.getConfig(EDC_DATASOURCE_PREFIX);
+
+        Map<String, CommonsConnectionPool> namedConnectionPools = createConnectionPools(config);
+
+        for (Map.Entry<String, CommonsConnectionPool> entry : namedConnectionPools.entrySet()) {
+            String dataSourceName = entry.getKey();
+            CommonsConnectionPool commonsConnectionPool = entry.getValue();
+            commonsConnectionPools.add(commonsConnectionPool);
+            ConnectionPoolDataSource connectionPoolDataSource = new ConnectionPoolDataSource(commonsConnectionPool);
+            dataSourceRegistry.register(dataSourceName, connectionPoolDataSource);
+        }
+    }
+
+    @Override
+    public void shutdown() {
+        for (CommonsConnectionPool commonsConnectionPool : commonsConnectionPools) {
+            commonsConnectionPool.close();
+        }
+    }
+
+    public List<CommonsConnectionPool> getCommonsConnectionPools() {
+        return commonsConnectionPools;
+    }
+
+    private void setIfProvidedString(String key, Consumer<String> setter, Config config) {
+        setIfProvided(key, setter, config::getString);
+    }
+
+    private void setIfProvidedBoolean(String key, Consumer<Boolean> setter, Config config) {
+        setIfProvided(key, setter, config::getBoolean);
+    }
+
+    private void setIfProvidedInt(String key, Consumer<Integer> setter, Config config) {
+        setIfProvided(key, setter, config::getInteger);
+    }
+
+    private <T> void setIfProvided(String key, Consumer<T> setter, BiFunction<String, T, T> getter) {
+        var oldKey = CONFIGURATION_MAPPING.get(key);
+        var oldValue = getter.apply(oldKey, null);
+        if (oldValue != null) {
+            monitor.warning(format("Configuration setting %s has been deprecated, please use %s instead", oldKey, key));
+        }
+        var value = getter.apply(key, oldValue);
+        if (value != null) {
+            setter.accept(value);
+        }
+    }
+
+    private Map<String, CommonsConnectionPool> createConnectionPools(Config parent) {
+        Map<String, CommonsConnectionPool> commonsConnectionPools = new HashMap<>();
+        for (Config config : parent.partition().toList()) {
+            String dataSourceName = config.currentNode();
+
+            DataSource dataSource = createDataSource(config);
+
+            CommonsConnectionPool commonsConnectionPool = createConnectionPool(dataSource, config);
+            commonsConnectionPools.put(dataSourceName, commonsConnectionPool);
+        }
+        return commonsConnectionPools;
+    }
+
+    private DataSource createDataSource(Config config) {
+        String jdbcUrl = Objects.requireNonNull(config.getString(URL));
+
+        Properties properties = new Properties();
+        properties.putAll(config.getRelativeEntries());
+
+        ConnectionFactory connectionFactory = new DriverManagerConnectionFactory(jdbcUrl, properties);
+
+        return new ConnectionFactoryDataSource(connectionFactory);
+    }
+
+    private CommonsConnectionPool createConnectionPool(DataSource unPooledDataSource, Config config) {
+        CommonsConnectionPoolConfig.Builder builder = CommonsConnectionPoolConfig.Builder.newInstance();
+
+        setIfProvidedInt(POOL_CONNECTIONS_MAX_IDLE, builder::maxIdleConnections, config);
+        setIfProvidedInt(POOL_CONNECTIONS_MAX_TOTAL, builder::maxTotalConnections, config);
+        setIfProvidedInt(POOL_CONNECTIONS_MIN_IDLE, builder::minIdleConnections, config);
+        setIfProvidedBoolean(POOL_CONNECTION_TEST_ON_BORROW, builder::testConnectionOnBorrow, config);
+        setIfProvidedBoolean(POOL_CONNECTION_TEST_ON_CREATE, builder::testConnectionOnCreate, config);
+        setIfProvidedBoolean(POOL_CONNECTION_TEST_ON_RETURN, builder::testConnectionOnReturn, config);
+        setIfProvidedBoolean(POOL_CONNECTION_TEST_WHILE_IDLE, builder::testConnectionWhileIdle, config);
+        setIfProvidedString(POOL_CONNECTION_TEST_QUERY, builder::testQuery, config);
+
+        return new CommonsConnectionPool(unPooledDataSource, builder.build(), monitor);
+    }
+}
---
+++ edc-extensions/sql-pool/src/test/java/org/eclipse/tractusx/edc/sql/pool/commons/CommonsConnectionPoolServiceExtensionTest.java
@@ -0,0 +1,160 @@
+/*
+ *  Copyright (c) 2023 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Bayerische Motoren Werke Aktiengesellschaft (BMW AG) - initial API and implementation
+ *
+ */
+
+package org.eclipse.tractusx.edc.sql.pool.commons;
+
+import org.assertj.core.api.ThrowingConsumer;
+import org.eclipse.edc.junit.extensions.DependencyInjectionExtension;
+import org.eclipse.edc.spi.system.ServiceExtensionContext;
+import org.eclipse.edc.spi.system.configuration.Config;
+import org.eclipse.edc.spi.system.configuration.ConfigFactory;
+import org.eclipse.edc.transaction.datasource.spi.DataSourceRegistry;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.eclipse.tractusx.edc.sql.pool.commons.CommonsConnectionPoolConfigKeys.DEPRACATED_POOL_MAX_IDLE_CONNECTIONS;
+import static org.eclipse.tractusx.edc.sql.pool.commons.CommonsConnectionPoolConfigKeys.DEPRACATED_POOL_MAX_TOTAL_CONNECTIONS;
+import static org.eclipse.tractusx.edc.sql.pool.commons.CommonsConnectionPoolConfigKeys.DEPRACATED_POOL_MIN_IDLE_CONNECTIONS;
+import static org.eclipse.tractusx.edc.sql.pool.commons.CommonsConnectionPoolConfigKeys.DEPRACATED_POOL_TEST_CONNECTION_ON_BORROW;
+import static org.eclipse.tractusx.edc.sql.pool.commons.CommonsConnectionPoolConfigKeys.DEPRACATED_POOL_TEST_CONNECTION_ON_CREATE;
+import static org.eclipse.tractusx.edc.sql.pool.commons.CommonsConnectionPoolConfigKeys.DEPRACATED_POOL_TEST_CONNECTION_ON_RETURN;
+import static org.eclipse.tractusx.edc.sql.pool.commons.CommonsConnectionPoolConfigKeys.DEPRACATED_POOL_TEST_CONNECTION_WHILE_IDLE;
+import static org.eclipse.tractusx.edc.sql.pool.commons.CommonsConnectionPoolConfigKeys.DEPRACATED_POOL_TEST_QUERY;
+import static org.eclipse.tractusx.edc.sql.pool.commons.CommonsConnectionPoolConfigKeys.POOL_CONNECTIONS_MAX_IDLE;
+import static org.eclipse.tractusx.edc.sql.pool.commons.CommonsConnectionPoolConfigKeys.POOL_CONNECTIONS_MAX_TOTAL;
+import static org.eclipse.tractusx.edc.sql.pool.commons.CommonsConnectionPoolConfigKeys.POOL_CONNECTIONS_MIN_IDLE;
+import static org.eclipse.tractusx.edc.sql.pool.commons.CommonsConnectionPoolConfigKeys.POOL_CONNECTION_TEST_ON_BORROW;
+import static org.eclipse.tractusx.edc.sql.pool.commons.CommonsConnectionPoolConfigKeys.POOL_CONNECTION_TEST_ON_CREATE;
+import static org.eclipse.tractusx.edc.sql.pool.commons.CommonsConnectionPoolConfigKeys.POOL_CONNECTION_TEST_ON_RETURN;
+import static org.eclipse.tractusx.edc.sql.pool.commons.CommonsConnectionPoolConfigKeys.POOL_CONNECTION_TEST_QUERY;
+import static org.eclipse.tractusx.edc.sql.pool.commons.CommonsConnectionPoolConfigKeys.POOL_CONNECTION_TEST_WHILE_IDLE;
+import static org.eclipse.tractusx.edc.sql.pool.commons.CommonsConnectionPoolServiceExtension.EDC_DATASOURCE_PREFIX;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+//sometimes hangs and causes the test to never finish.
+@ExtendWith(DependencyInjectionExtension.class)
+class CommonsConnectionPoolServiceExtensionTest {
+    private static final String DS_1_NAME = "ds1";
+    private final DataSourceRegistry dataSourceRegistry = mock();
+
+    @BeforeEach
+    void setUp(ServiceExtensionContext context) {
+        context.registerService(DataSourceRegistry.class, dataSourceRegistry);
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(ConfigProvider.class)
+    void initialize_withConfig(Map<String, String> configuration, ThrowingConsumer<CommonsConnectionPoolConfig> checker,
+                               boolean isEnv,
+                               CommonsConnectionPoolServiceExtension extension, ServiceExtensionContext context) {
+        Config config;
+        if (isEnv) {
+            config = ConfigFactory.fromEnvironment(configuration);
+        } else {
+            config = ConfigFactory.fromMap(configuration);
+        }
+        when(context.getConfig(EDC_DATASOURCE_PREFIX)).thenReturn(config);
+
+        extension.initialize(context);
+
+        verify(dataSourceRegistry).register(eq(DS_1_NAME), any());
+
+        assertThat(extension.getCommonsConnectionPools()).hasSize(1).first()
+                .extracting(CommonsConnectionPool::getPoolConfig)
+                .satisfies(checker);
+    }
+
+
+    static class ConfigProvider implements ArgumentsProvider {
+
+        private final Map<String, String> defaultConfig = Map.of(DS_1_NAME + ".url", DS_1_NAME);
+
+        private final Map<String, String> configuration = Map.of(
+                DS_1_NAME + ".url", DS_1_NAME,
+                DS_1_NAME + "." + POOL_CONNECTION_TEST_ON_CREATE, "false",
+                DS_1_NAME + "." + POOL_CONNECTION_TEST_ON_BORROW, "false",
+                DS_1_NAME + "." + POOL_CONNECTION_TEST_ON_RETURN, "true",
+                DS_1_NAME + "." + POOL_CONNECTION_TEST_WHILE_IDLE, "true",
+                DS_1_NAME + "." + POOL_CONNECTION_TEST_QUERY, "SELECT foo FROM bar;",
+                DS_1_NAME + "." + POOL_CONNECTIONS_MIN_IDLE, "10",
+                DS_1_NAME + "." + POOL_CONNECTIONS_MAX_IDLE, "10",
+                DS_1_NAME + "." + POOL_CONNECTIONS_MAX_TOTAL, "10");
+
+
+        private final Map<String, String> deprecatedConfig = Map.of(
+                DS_1_NAME + ".url", DS_1_NAME,
+                DS_1_NAME + "." + DEPRACATED_POOL_TEST_CONNECTION_ON_CREATE, "false",
+                DS_1_NAME + "." + DEPRACATED_POOL_TEST_CONNECTION_ON_BORROW, "false",
+                DS_1_NAME + "." + DEPRACATED_POOL_TEST_CONNECTION_ON_RETURN, "true",
+                DS_1_NAME + "." + DEPRACATED_POOL_TEST_CONNECTION_WHILE_IDLE, "true",
+                DS_1_NAME + "." + DEPRACATED_POOL_TEST_QUERY, "SELECT foo FROM bar;",
+                DS_1_NAME + "." + DEPRACATED_POOL_MIN_IDLE_CONNECTIONS, "10",
+                DS_1_NAME + "." + DEPRACATED_POOL_MAX_IDLE_CONNECTIONS, "10",
+                DS_1_NAME + "." + DEPRACATED_POOL_MAX_TOTAL_CONNECTIONS, "10");
+
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+            ThrowingConsumer<CommonsConnectionPoolConfig> checkDefault = this::checkDefault;
+            ThrowingConsumer<CommonsConnectionPoolConfig> checkWithConfig = this::checkWithConfig;
+
+            var envConfiguration = configuration.entrySet().stream()
+                    .map(it -> Map.entry(it.getKey().toUpperCase().replace(".", "_"), it.getValue()))
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+            return Stream.of(
+                    Arguments.of(defaultConfig, checkDefault, false),
+                    Arguments.of(configuration, checkWithConfig, false),
+                    Arguments.of(envConfiguration, checkWithConfig, true),
+                    Arguments.of(deprecatedConfig, checkWithConfig, false)
+            );
+        }
+
+        private void checkDefault(CommonsConnectionPoolConfig cfg) {
+            assertThat(cfg.getTestConnectionOnCreate()).isTrue();
+            assertThat(cfg.getTestConnectionOnBorrow()).isTrue();
+            assertThat(cfg.getTestConnectionOnReturn()).isFalse();
+            assertThat(cfg.getTestConnectionWhileIdle()).isFalse();
+            assertThat(cfg.getTestQuery()).isEqualTo("SELECT 1;");
+            assertThat(cfg.getMinIdleConnections()).isEqualTo(1);
+            assertThat(cfg.getMaxIdleConnections()).isEqualTo(4);
+            assertThat(cfg.getMaxTotalConnections()).isEqualTo(8);
+        }
+
+        private void checkWithConfig(CommonsConnectionPoolConfig cfg) {
+            assertThat(cfg.getTestConnectionOnCreate()).isFalse();
+            assertThat(cfg.getTestConnectionOnBorrow()).isFalse();
+            assertThat(cfg.getTestConnectionOnReturn()).isTrue();
+            assertThat(cfg.getTestConnectionWhileIdle()).isTrue();
+            assertThat(cfg.getTestQuery()).isEqualTo("SELECT foo FROM bar;");
+            assertThat(cfg.getMinIdleConnections()).isEqualTo(10);
+            assertThat(cfg.getMaxIdleConnections()).isEqualTo(10);
+            assertThat(cfg.getMaxTotalConnections()).isEqualTo(10);
+        }
+
+    }
+
+}
