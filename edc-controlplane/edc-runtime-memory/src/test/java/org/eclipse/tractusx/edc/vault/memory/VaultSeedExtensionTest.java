796ae72015969789141bbd8eab612be1f6e40613
Rafael Magalhaes <rafaelmagftw@gmail.com>
2024-02-08 10:27:09 +0000

    chore: fix missing imports

--- edc-controlplane/edc-runtime-memory/src/test/java/org/eclipse/tractusx/edc/vault/memory/VaultSeedExtensionTest.java
+++ edc-controlplane/edc-runtime-memory/src/test/java/org/eclipse/tractusx/edc/vault/memory/VaultSeedExtensionTest.java
@@ -24,6 +24,7 @@ import org.eclipse.edc.junit.extensions.DependencyInjectionExtension;
 import org.eclipse.edc.spi.monitor.Monitor;
 import org.eclipse.edc.spi.security.Vault;
 import org.eclipse.edc.spi.system.ServiceExtensionContext;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -42,7 +43,7 @@ class VaultSeedExtensionTest {
     private Monitor monitor;
 
     @BeforeEach
-    void setup(ServiceExtensionContext context, ObjectFactory factory) {
+    void setup(ServiceExtensionContext context) {
         monitor = mock(Monitor.class);
         context.registerService(Monitor.class, monitor);
         context.registerService(Vault.class, new InMemoryVault(monitor));
@@ -54,7 +55,7 @@ class VaultSeedExtensionTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"key1:", "key1:value1", "key1:value1;", ";key1:value1", ";sdf;key1:value1"})
+    @ValueSource(strings = { "key1:", "key1:value1", "key1:value1;", ";key1:value1", ";sdf;key1:value1" })
     void createInMemVault_validString(String secret, ServiceExtensionContext context, VaultSeedExtension extension) {
         when(context.getSetting(eq(VaultSeedExtension.VAULT_MEMORY_SECRETS_PROPERTY), eq(null))).thenReturn(secret);
         extension.createInMemVault(context);
