73b06c5c857234585e971938bd8a8525db6ddde2
Enrico Risa <enrico.risa@gmail.com>
2024-02-15 18:10:15 +0100

    chore: removes temporary credentials functions

--- edc-tests/runtime/iatp/iatp-extensions/src/main/java/org/eclipse/tractusx/edc/iatp/policy/IdentityExtractor.java
+++ edc-tests/runtime/iatp/iatp-extensions/src/main/java/org/eclipse/tractusx/edc/iatp/policy/IdentityExtractor.java
@@ -19,25 +19,68 @@
 
 package org.eclipse.tractusx.edc.iatp.policy;
 
+import org.eclipse.edc.identitytrust.model.VerifiableCredential;
+import org.eclipse.edc.spi.EdcException;
 import org.eclipse.edc.spi.agent.ParticipantAgentServiceExtension;
 import org.eclipse.edc.spi.iam.ClaimToken;
+import org.eclipse.edc.spi.result.Result;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import static org.eclipse.edc.spi.agent.ParticipantAgent.PARTICIPANT_IDENTITY;
 
+// TODO this is just a test identity extractor, the real one can be inspired by this but with final Identity credential
 public class IdentityExtractor implements ParticipantAgentServiceExtension {
+
+    private static final String VC_CLAIM = "vc";
+    private static final String IDENTITY_CREDENTIAL = "MembershipCredential";
+    private static final String IDENTITY_PROPERTY = "holderIdentifier";
+
     @Override
     public @NotNull Map<String, String> attributesFor(ClaimToken claimToken) {
-        return Map.of(PARTICIPANT_IDENTITY, getClaim(String.class, "holderIdentifier", claimToken.getClaims()));
+        var credentials = getCredentialList(claimToken)
+                .orElseThrow(failure -> new EdcException("Failed to fetch credentials from the claim token: %s".formatted(failure.getFailureDetail())));
+
+        return credentials.stream()
+                .filter(this::isIdentityCredential)
+                .findFirst()
+                .flatMap(this::getIdentifier)
+                .map(identity -> Map.of(PARTICIPANT_IDENTITY, identity))
+                .orElseThrow(() -> new EdcException("Failed to fetch %s property from %s credential".formatted(IDENTITY_PROPERTY, IDENTITY_CREDENTIAL)));
+
+
     }
 
-    protected <T> T getClaim(Class<T> type, String postfix, Map<String, Object> claims) {
-        return claims.entrySet().stream().filter(e -> e.getKey().endsWith(postfix))
-                .findFirst()
+    private boolean isIdentityCredential(VerifiableCredential verifiableCredential) {
+        return verifiableCredential.getTypes().stream().anyMatch(t -> t.endsWith(IDENTITY_CREDENTIAL));
+    }
+
+    private Optional<String> getIdentifier(VerifiableCredential verifiableCredential) {
+        return verifiableCredential.getCredentialSubject().stream()
+                .flatMap(credentialSubject -> credentialSubject.getClaims().entrySet().stream())
+                .filter(entry -> entry.getKey().endsWith(IDENTITY_PROPERTY))
                 .map(Map.Entry::getValue)
-                .map(type::cast)
-                .orElse(null);
+                .map(String.class::cast)
+                .findFirst();
+    }
+
+    @SuppressWarnings("unchecked")
+    private Result<List<VerifiableCredential>> getCredentialList(ClaimToken claimToken) {
+        var vcListClaim = claimToken.getClaims().get(VC_CLAIM);
+
+        if (vcListClaim == null) {
+            return Result.failure("ClaimToken did not contain a '%s' claim.".formatted(VC_CLAIM));
+        }
+        if (!(vcListClaim instanceof List)) {
+            return Result.failure("ClaimToken contains a '%s' claim, but the type is incorrect. Expected %s, got %s.".formatted(VC_CLAIM, List.class.getName(), vcListClaim.getClass().getName()));
+        }
+        var vcList = (List<VerifiableCredential>) vcListClaim;
+        if (vcList.isEmpty()) {
+            return Result.failure("ClaimToken contains a '%s' claim but it did not contain any VerifiableCredentials.".formatted(VC_CLAIM));
+        }
+        return Result.success(vcList);
     }
 }
