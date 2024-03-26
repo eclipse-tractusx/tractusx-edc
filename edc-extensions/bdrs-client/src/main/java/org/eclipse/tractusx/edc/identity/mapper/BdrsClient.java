ae87b0a33e12a867b394b987768dbaa468c135c3
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2024-03-26 16:47:04 +0100

    feat: add BDRS client w/ expiring cache

---
+++ edc-extensions/bdrs-client/src/main/java/org/eclipse/tractusx/edc/identity/mapper/BdrsClient.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2024 Bayerische Motoren Werke Aktiengesellschaft
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.eclipse.tractusx.edc.identity.mapper;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import okhttp3.Request;
+import org.eclipse.edc.spi.EdcException;
+import org.eclipse.edc.spi.http.EdcHttpClient;
+import org.eclipse.edc.spi.iam.AudienceResolver;
+import org.eclipse.edc.spi.monitor.Monitor;
+import org.eclipse.edc.spi.types.domain.message.RemoteMessage;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Map;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.zip.GZIPInputStream;
+
+public class BdrsClient implements AudienceResolver {
+    private static final TypeReference<Map<String, String>> MAP_REF = new TypeReference<>() {
+    };
+    private final String serverUrl;
+    private final int cacheValidity;
+    private final EdcHttpClient httpClient;
+    private final Monitor monitor;
+    private final ObjectMapper mapper;
+    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+    private Instant lastCacheUpdate;
+    private Map<String, String> cache;
+
+    public BdrsClient(String baseUrl, int cacheValidity, EdcHttpClient httpClient, Monitor monitor, ObjectMapper mapper) {
+        this.serverUrl = baseUrl;
+        this.cacheValidity = cacheValidity;
+        this.httpClient = httpClient;
+        this.monitor = monitor;
+        this.mapper = mapper;
+    }
+
+    @Override
+    public String resolve(RemoteMessage remoteMessage) {
+        var bpn = remoteMessage.getCounterPartyId();
+        String value;
+        lock.readLock().lock();
+        if (isCacheExpired()) {
+            lock.readLock().unlock(); // unlock read, acquire write -> "upgrade" lock
+            lock.writeLock().lock();
+            try {
+                if (isCacheExpired()) {
+                    updateCache();
+                }
+                lock.readLock().lock(); // downgrade lock
+            } finally {
+                lock.writeLock().unlock();
+            }
+        }
+        try {
+            value = cache.get(bpn);
+        } finally {
+            lock.readLock().unlock();
+        }
+        return value;
+    }
+
+    private boolean isCacheExpired() {
+        return cache == null || lastCacheUpdate == null || lastCacheUpdate.plus(cacheValidity, ChronoUnit.SECONDS).isBefore(Instant.now());
+    }
+
+    private void updateCache() {
+        var request = new Request.Builder()
+                //.addHeader("Authorization", createMembershipPresentation()) //todo: add MembershipCredential as JWT-VP to the auth header
+                .header("Accept-Encoding", "gzip")
+                .url(serverUrl + "/bpn-directory")
+                .get()
+                .build();
+        try (var response = httpClient.execute(request)) {
+            if (response.isSuccessful() && response.body() != null) {
+                var body = response.body().byteStream();
+                var bytes = new GZIPInputStream(body).readAllBytes();
+                cache = mapper.readValue(bytes, MAP_REF);
+                lastCacheUpdate = Instant.now();
+            } else {
+                var msg = "Could not obtain data from BDRS server: code: %d, message: %s".formatted(response.code(), response.message());
+                throw new EdcException(msg);
+            }
+        } catch (IOException e) {
+            monitor.severe("Error fetching BDRS data", e);
+            throw new EdcException(e);
+        }
+    }
+
+}
---
+++ edc-extensions/bdrs-client/src/test/java/org/eclipse/tractusx/edc/identity/mapper/BdrsClientTest.java
@@ -0,0 +1,173 @@
+/*
+ * Copyright (c) 2024 Bayerische Motoren Werke Aktiengesellschaft
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.eclipse.tractusx.edc.identity.mapper;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.failsafe.RetryPolicy;
+import okhttp3.OkHttpClient;
+import org.eclipse.edc.spi.EdcException;
+import org.eclipse.edc.spi.monitor.Monitor;
+import org.eclipse.edc.spi.types.domain.message.RemoteMessage;
+import org.eclipse.http.client.EdcHttpClientImpl;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockserver.integration.ClientAndServer;
+import org.mockserver.model.HttpResponse;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Map;
+import java.util.zip.GZIPOutputStream;
+
+import static java.time.Duration.ofSeconds;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.awaitility.Awaitility.await;
+import static org.eclipse.edc.util.io.Ports.getFreePort;
+import static org.mockito.Mockito.mock;
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.verify.VerificationTimes.exactly;
+
+class BdrsClientTest {
+
+    private final Monitor monitor = mock();
+    private final ObjectMapper mapper = new ObjectMapper();
+    private BdrsClient client;
+    private ClientAndServer bdrsServer;
+
+    @BeforeEach
+    void setup() {
+        bdrsServer = ClientAndServer.startClientAndServer(getFreePort());
+        bdrsServer.when(request()
+                        .withMethod("GET")
+                        .withPath("/api/bpn-directory"))
+                .respond(HttpResponse.response()
+                        .withHeader("Content-Encoding", "gzip")
+                        .withBody(createGzipStream())
+                        .withStatusCode(200));
+
+        client = new BdrsClient("http://localhost:%d/api".formatted(bdrsServer.getPort()), 1, new EdcHttpClientImpl(new OkHttpClient(), RetryPolicy.ofDefaults(), monitor), monitor, mapper);
+    }
+
+    @AfterEach
+    void teardown() {
+        bdrsServer.stop();
+    }
+
+    @Test
+    void getData_whenCacheCold_shouldHitServer() {
+        var did = client.resolve(new TestMessage("bpn1"));
+        assertThat(did).isEqualTo("did:web:did1");
+
+        bdrsServer.verify(request()
+                        .withMethod("GET")
+                        .withPath("/api/bpn-directory")
+                        .withHeader("Accept-Encoding", "gzip"),
+                exactly(1));
+    }
+
+    @Test
+    void getData_whenCacheHot_shouldNotHitServer() {
+        var did1 = client.resolve(new TestMessage("bpn1"));
+        var did2 = client.resolve(new TestMessage("bpn2"));
+        assertThat(did1).isEqualTo("did:web:did1");
+        assertThat(did2).isEqualTo("did:web:did2");
+
+        bdrsServer.verify(request()
+                        .withMethod("GET")
+                        .withPath("/api/bpn-directory")
+                        .withHeader("Accept-Encoding", "gzip"),
+                exactly(1));
+    }
+
+    @Test
+    void getData_whenCacheExpired_shouldHitServer() {
+        var did1 = client.resolve(new TestMessage("bpn1")); // hits server
+        assertThat(did1).isEqualTo("did:web:did1");
+
+        await().pollDelay(ofSeconds(2))
+                .atMost(ofSeconds(3)) //cache expires
+                .untilAsserted(() -> {
+                    var did2 = client.resolve(new TestMessage("bpn2")); // hits server as well, b/c cache is expired
+                    assertThat(did2).isEqualTo("did:web:did2");
+
+                    bdrsServer.verify(request()
+                                    .withHeader("Accept-Encoding", "gzip")
+                                    .withMethod("GET")
+                                    .withPath("/api/bpn-directory"),
+                            exactly(2));
+                });
+
+    }
+
+    @Test
+    void getData_whenNotFound() {
+        var did = client.resolve(new TestMessage("bpn-notexist"));
+        assertThat(did).isNull();
+        bdrsServer.verify(request()
+                        .withMethod("GET")
+                        .withPath("/api/bpn-directory")
+                        .withHeader("Accept-Encoding", "gzip"),
+                exactly(1));
+    }
+
+    @ParameterizedTest(name = "HTTP Status {0}")
+    @ValueSource(ints = { 400, 401, 403, 404, 405 })
+    void getData_bdrsReturnsError(int code) {
+        bdrsServer.reset();
+        bdrsServer.when(request().withPath("/api/bpn-directory").withMethod("GET"))
+                .respond(HttpResponse.response().withStatusCode(code));
+        assertThatThrownBy(() -> client.resolve(new TestMessage("bpn1"))).isInstanceOf(EdcException.class);
+    }
+
+    private byte[] createGzipStream() {
+        var data = Map.of("bpn1", "did:web:did1",
+                "bpn2", "did:web:did2",
+                "bpn3", "did:web:did3");
+
+        var bas = new ByteArrayOutputStream();
+        try (var gzip = new GZIPOutputStream(bas)) {
+            gzip.write(mapper.writeValueAsBytes(data));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return bas.toByteArray();
+    }
+
+    private record TestMessage(String bpn) implements RemoteMessage {
+        @Override
+        public String getProtocol() {
+            return "test-proto";
+        }
+
+        @Override
+        public String getCounterPartyAddress() {
+            return "http://bpn1";
+        }
+
+        @Override
+        public String getCounterPartyId() {
+            return bpn;
+        }
+    }
+}
