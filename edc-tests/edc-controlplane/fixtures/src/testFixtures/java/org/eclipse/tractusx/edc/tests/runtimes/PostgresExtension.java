d0c2e268b140fe4b9b09f0bcf51a792c0a834592
andrea bertagnolli <andrea.bertagnolli@gmail.com>
2024-09-25 14:31:41 +0200

    refactor(test): extract PostgresExtension (#1583)

--- edc-tests/edc-controlplane/fixtures/src/testFixtures/java/org/eclipse/tractusx/edc/tests/runtimes/PostgresExtension.java
+++ edc-tests/edc-controlplane/fixtures/src/testFixtures/java/org/eclipse/tractusx/edc/tests/runtimes/PostgresExtension.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Bayerische Motoren Werke Aktiengesellschaft
+ * Copyright (c) 2024 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
@@ -19,8 +19,9 @@
 
 package org.eclipse.tractusx.edc.tests.runtimes;
 
-import com.github.dockerjava.api.model.ExposedPort;
 import org.eclipse.edc.util.io.Ports;
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
@@ -31,51 +32,46 @@ import java.util.List;
 import java.util.Map;
 
 import static java.lang.String.format;
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.joining;
 import static org.eclipse.tractusx.edc.tests.TestRuntimeConfiguration.DB_SCHEMA_NAME;
 
 /**
- * Instantiates the Postgres docker container and configures the runtime accordingly
+ * JUnit extension that permits to spin up a PostgresSQL container
  */
-public class PgRuntimeExtension extends ParticipantRuntimeExtension {
+public class PostgresExtension implements BeforeAllCallback, AfterAllCallback {
+
     private static final String POSTGRES_IMAGE_NAME = "postgres:16.4";
     private static final String USER = "postgres";
     private static final String PASSWORD = "password";
     private final PostgreSQLContainer<?> postgreSqlContainer;
-    private final String dbName;
+    private final String[] databases;
+    private final int exposedPort;
 
-    public PgRuntimeExtension(String moduleName, String runtimeName, String bpn, Map<String, String> properties) {
-        super(moduleName, runtimeName, bpn, properties);
-        this.dbName = runtimeName.toLowerCase();
-        postgreSqlContainer = new PostgreSQLContainer<>(POSTGRES_IMAGE_NAME)
-                .withLabel("runtime", dbName)
+    public PostgresExtension(String... databases) {
+        this.databases = databases;
+        exposedPort = Ports.getFreePort();
+        this.postgreSqlContainer = new PostgreSQLContainer<>(POSTGRES_IMAGE_NAME)
                 .withUsername(USER)
-                .withPassword(PASSWORD)
-                .withDatabaseName(dbName);
-
-        postgreSqlContainer.setPortBindings(List.of("%d:5432".formatted(Ports.getFreePort())));
-
+                .withPassword(PASSWORD);
+        postgreSqlContainer.setPortBindings(List.of("%d:5432".formatted(exposedPort)));
     }
 
     @Override
     public void beforeAll(ExtensionContext context) {
-
         postgreSqlContainer.start();
         postgreSqlContainer.waitingFor(Wait.forHealthcheck());
-        var config = postgresqlConfiguration(dbName);
-        config.forEach(System::setProperty);
-        createDatabase();
-        super.beforeAll(context);
+        this.createDatabases();
     }
 
     @Override
     public void afterAll(ExtensionContext context) {
-        super.afterAll(context);
         postgreSqlContainer.stop();
         postgreSqlContainer.close();
     }
 
-    private Map<String, String> postgresqlConfiguration(String name) {
-        var jdbcUrl = baseJdbcUrl() + name + "?currentSchema=" + DB_SCHEMA_NAME;
+    public Map<String, String> getConfiguration(String databaseName) {
+        var jdbcUrl = baseJdbcUrl() + databaseName.toLowerCase() + "?currentSchema=" + DB_SCHEMA_NAME;
         var group = "edc.datasource.default";
 
         return Map.of(
@@ -86,15 +82,16 @@ public class PgRuntimeExtension extends ParticipantRuntimeExtension {
         );
     }
 
-    private void createDatabase() {
+    private void createDatabases() {
         try (var connection = DriverManager.getConnection(baseJdbcUrl() + "postgres", postgreSqlContainer.getUsername(), postgreSqlContainer.getPassword())) {
-            connection.createStatement().execute(String.format("create database %s;", postgreSqlContainer.getDatabaseName()));
+            var command = stream(databases).map("create database %s;"::formatted).collect(joining("; "));
+            connection.createStatement().execute(command);
         } catch (SQLException ignored) {
             // ignored
         }
     }
 
     private String baseJdbcUrl() {
-        return format("jdbc:postgresql://%s:%s/", postgreSqlContainer.getHost(), postgreSqlContainer.getFirstMappedPort());
+        return format("jdbc:postgresql://%s:%s/", postgreSqlContainer.getHost(), exposedPort);
     }
 }
--- edc-tests/edc-controlplane/transfer-tests/src/test/java/org/eclipse/tractusx/edc/tests/transfer/TransferPushEndToEndTest.java
+++ edc-tests/edc-controlplane/transfer-tests/src/test/java/org/eclipse/tractusx/edc/tests/transfer/TransferPushEndToEndTest.java
@@ -24,7 +24,9 @@ import org.eclipse.edc.junit.annotations.PostgresqlIntegrationTest;
 import org.eclipse.edc.junit.extensions.RuntimeExtension;
 import org.eclipse.tractusx.edc.tests.participant.TractusxParticipantBase;
 import org.eclipse.tractusx.edc.tests.participant.TransferParticipant;
+import org.eclipse.tractusx.edc.tests.runtimes.PostgresExtension;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 import static org.eclipse.tractusx.edc.tests.TestRuntimeConfiguration.CONSUMER_BPN;
@@ -76,10 +78,14 @@ public class TransferPushEndToEndTest {
     class Postgres extends Tests {
 
         @RegisterExtension
-        protected static final RuntimeExtension CONSUMER_RUNTIME = pgRuntime(CONSUMER.getName(), CONSUMER.getBpn(), CONSUMER.getConfiguration());
+        @Order(0)
+        private static final PostgresExtension POSTGRES = new PostgresExtension(CONSUMER.getName(), PROVIDER.getName());
 
         @RegisterExtension
-        protected static final RuntimeExtension PROVIDER_RUNTIME = pgRuntime(PROVIDER.getName(), PROVIDER.getBpn(), PROVIDER.getConfiguration());
+        protected static final RuntimeExtension CONSUMER_RUNTIME = pgRuntime(CONSUMER, POSTGRES);
+
+        @RegisterExtension
+        protected static final RuntimeExtension PROVIDER_RUNTIME = pgRuntime(PROVIDER, POSTGRES);
 
     }
 }
