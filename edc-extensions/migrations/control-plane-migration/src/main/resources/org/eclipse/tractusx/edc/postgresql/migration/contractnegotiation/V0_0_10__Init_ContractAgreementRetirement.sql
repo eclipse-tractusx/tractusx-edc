828f68c7a2266d4eaaa2d29b0f7db29c42ea0313
bmg13 <gabcmonteiro@gmail.com>
2024-10-17 15:57:14 +0100

    Changes from PR.

--- edc-extensions/migrations/control-plane-migration/src/main/resources/org/eclipse/tractusx/edc/postgresql/migration/contractnegotiation/V0_0_10__Init_ContractAgreementRetirement.sql
+++ edc-extensions/migrations/control-plane-migration/src/main/resources/org/eclipse/tractusx/edc/postgresql/migration/contractnegotiation/V0_0_10__Init_ContractAgreementRetirement.sql
@@ -12,24 +12,6 @@
 --
 
 --
--- table: edc_lease
---
-CREATE TABLE IF NOT EXISTS edc_lease
-(
-    leased_by      VARCHAR(255)          NOT NULL,
-    leased_at      BIGINT,
-    lease_duration INTEGER DEFAULT 60000 NOT NULL,
-    lease_id       VARCHAR(255)          NOT NULL
-        CONSTRAINT lease_pk
-            PRIMARY KEY
-);
-COMMENT ON COLUMN edc_lease.leased_at IS 'posix timestamp of lease';
-COMMENT ON COLUMN edc_lease.lease_duration IS 'duration of lease in milliseconds';
-
-CREATE UNIQUE INDEX IF NOT EXISTS lease_lease_id_uindex
-    ON edc_lease (lease_id);
-
---
 -- table: edc_contract_agreement
 --
 CREATE TABLE IF NOT EXISTS edc_contract_agreement
@@ -44,30 +26,6 @@ CREATE TABLE IF NOT EXISTS edc_contract_agreement
     asset_id          VARCHAR(255) NOT NULL,
     policy_id         VARCHAR(255),
     serialized_policy TEXT
-);
-
---
--- table: edc_contract_negotiation
---
-CREATE TABLE IF NOT EXISTS edc_contract_negotiation
-(
-    id                    VARCHAR(255)                                             NOT NULL
-        CONSTRAINT contract_negotiation_pk PRIMARY KEY,
-    correlation_id        VARCHAR(255)                                             NOT NULL,
-    counterparty_id       VARCHAR(255)                                             NOT NULL,
-    counterparty_address  VARCHAR(255)                                             NOT NULL,
-    protocol              VARCHAR(255) DEFAULT 'ids-multipart':: CHARACTER VARYING NOT NULL,
-    type                  INTEGER      DEFAULT 0                                   NOT NULL,
-    state                 INTEGER      DEFAULT 0                                   NOT NULL,
-    state_count           INTEGER      DEFAULT 0,
-    state_timestamp       BIGINT,
-    error_detail          TEXT,
-    contract_agreement_id TEXT
-        CONSTRAINT contract_negotiation_contract_agreement_id_fk REFERENCES edc_contract_agreement,
-    contract_offers       TEXT,
-    trace_context         TEXT,
-    lease_id              VARCHAR(255)
-        CONSTRAINT contract_negotiation_lease_lease_id_fk REFERENCES edc_lease ON DELETE SET NULL
 );
 
 CREATE TABLE IF NOT EXISTS edc_agreement_retirement
