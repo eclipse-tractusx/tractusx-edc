bbabc17e17ef037a1479acf6befe23a25b60cc09
Rafael Magalhaes <rafaelmagftw@gmail.com>
2025-02-04 18:25:54 +0000

    this was actually needed

--- edc-tests/edc-controlplane/validator-tests/build.gradle.kts
+++ edc-tests/edc-controlplane/validator-tests/build.gradle.kts
@@ -23,6 +23,7 @@ plugins {
 }
 
 dependencies {
+    testImplementation(testFixtures(project(":edc-tests:e2e-fixtures")))
 
     testImplementation(libs.edc.junit)
     testImplementation(libs.restAssured)
