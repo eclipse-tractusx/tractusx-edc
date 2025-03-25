fa6359b343813f83231b0925150a847f81968f3d
Farhin Jahan <117462588+farhin23@users.noreply.github.com>
2025-03-25 11:12:30 +0100

    Add dataplane proxy public api extension module (#1840)
    * Add dataplane public api extension module
    
    * Update DEPENDENCIES

---
+++ edc-extensions/dataplane/dataplane-proxy/dataplane-public-api-v2/build.gradle.kts
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2022 Microsoft Corporation - initial API and implementation
+ * Copyright (c) 2024 Bayerische Motoren Werke Aktiengesellschaft (BMW AG) - improvements
+ * Copyright (c) 2024 Mercedes-Benz Tech Innovation GmbH - publish public api context into dedicated swagger hub page
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ **/
+
+plugins {
+    `java-library`
+    id(libs.plugins.swagger.get().pluginId)
+}
+
+dependencies {
+    api(libs.edc.spi.http)
+    api(libs.edc.spi.web)
+    api(libs.edc.spi.dataplane.dataplane)
+    implementation(libs.edc.lib.util)
+
+    implementation(libs.edc.dpf.util)
+    implementation(libs.jakarta.rsApi)
+
+    testImplementation(libs.edc.ext.http)
+    testImplementation(libs.edc.junit)
+    testImplementation(libs.edc.core.jersey)
+
+    testImplementation(libs.restAssured)
+    testImplementation(libs.netty.mockserver)
+    testImplementation(testFixtures(libs.edc.core.jersey))
+}
+edcBuild {
+    swagger {
+        apiGroup.set("public-api")
+    }
+}
+
+
---
+++ edc-extensions/dataplane/dataplane-proxy/dataplane-public-api-v2/src/main/java/org/eclipse/tractusx/edc/dataplane/proxy/api/DataPlanePublicApiV2Extension.java
@@ -0,0 +1,115 @@
+/**
+ * Copyright (c) 2024 Bayerische Motoren Werke Aktiengesellschaft (BMW AG) - initial API and implementation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ **/
+
+package org.eclipse.tractusx.edc.dataplane.proxy.api;
+
+import org.eclipse.edc.connector.dataplane.spi.Endpoint;
+import org.eclipse.edc.connector.dataplane.spi.iam.DataPlaneAuthorizationService;
+import org.eclipse.edc.connector.dataplane.spi.iam.PublicEndpointGeneratorService;
+import org.eclipse.edc.connector.dataplane.spi.pipeline.PipelineService;
+import org.eclipse.edc.runtime.metamodel.annotation.Configuration;
+import org.eclipse.edc.runtime.metamodel.annotation.Extension;
+import org.eclipse.edc.runtime.metamodel.annotation.Inject;
+import org.eclipse.edc.runtime.metamodel.annotation.Setting;
+import org.eclipse.edc.runtime.metamodel.annotation.Settings;
+import org.eclipse.edc.spi.system.ExecutorInstrumentation;
+import org.eclipse.edc.spi.system.Hostname;
+import org.eclipse.edc.spi.system.ServiceExtension;
+import org.eclipse.edc.spi.system.ServiceExtensionContext;
+import org.eclipse.edc.web.spi.WebService;
+import org.eclipse.edc.web.spi.configuration.ApiContext;
+import org.eclipse.edc.web.spi.configuration.PortMapping;
+import org.eclipse.edc.web.spi.configuration.PortMappingRegistry;
+import org.eclipse.tractusx.edc.dataplane.proxy.api.controller.DataPlanePublicApiV2Controller;
+
+import java.util.concurrent.Executors;
+
+/**
+ * This extension provides generic endpoints which are open to public participants of the Dataspace to execute
+ * requests on the actual data source.
+ */
+@Extension(value = DataPlanePublicApiV2Extension.NAME)
+public class DataPlanePublicApiV2Extension implements ServiceExtension {
+    public static final String NAME = "Data Plane Public API";
+
+    private static final int DEFAULT_PUBLIC_PORT = 8185;
+    private static final String DEFAULT_PUBLIC_PATH = "/api/public";
+    private static final int DEFAULT_THREAD_POOL = 10;
+    @Setting(description = "Base url of the public API endpoint without the trailing slash. This should point to the public endpoint configured.",
+            required = false,
+            key = "edc.dataplane.api.public.baseurl", warnOnMissingConfig = true)
+    private String publicBaseUrl;
+    @Setting(description = "Optional base url of the response channel endpoint without the trailing slash. A common practice is to use <PUBLIC_ENDPOINT>/responseChannel", key = "edc.dataplane.api.public.response.baseurl", required = false)
+    private String publicApiResponseUrl;
+    @Configuration
+    private PublicApiConfiguration apiConfiguration;
+    @Inject
+    private PortMappingRegistry portMappingRegistry;
+    @Inject
+    private PipelineService pipelineService;
+    @Inject
+    private WebService webService;
+    @Inject
+    private ExecutorInstrumentation executorInstrumentation;
+    @Inject
+    private DataPlaneAuthorizationService authorizationService;
+    @Inject
+    private PublicEndpointGeneratorService generatorService;
+    @Inject
+    private Hostname hostname;
+
+    @Override
+    public String name() {
+        return NAME;
+    }
+
+    @Override
+    public void initialize(ServiceExtensionContext context) {
+        var portMapping = new PortMapping(ApiContext.PUBLIC, apiConfiguration.port(), apiConfiguration.path());
+        portMappingRegistry.register(portMapping);
+        var executorService = executorInstrumentation.instrument(
+                Executors.newFixedThreadPool(DEFAULT_THREAD_POOL),
+                "Data plane proxy transfers"
+        );
+
+        if (publicBaseUrl == null) {
+            publicBaseUrl = "http://%s:%d%s".formatted(hostname.get(), portMapping.port(), portMapping.path());
+            context.getMonitor().warning("The public API endpoint was not explicitly configured, the default '%s' will be used.".formatted(publicBaseUrl));
+        }
+        var endpoint = Endpoint.url(publicBaseUrl);
+        generatorService.addGeneratorFunction("HttpData", dataAddress -> endpoint);
+
+        if (publicApiResponseUrl != null) {
+            generatorService.addGeneratorFunction("HttpData", () -> Endpoint.url(publicApiResponseUrl));
+        }
+
+        var publicApiController = new DataPlanePublicApiV2Controller(pipelineService, executorService, authorizationService);
+        webService.registerResource(ApiContext.PUBLIC, publicApiController);
+    }
+
+    @Settings
+    record PublicApiConfiguration(
+            @Setting(key = "web.http." + ApiContext.PUBLIC + ".port", description = "Port for " + ApiContext.PUBLIC + " api context", defaultValue = DEFAULT_PUBLIC_PORT + "")
+            int port,
+            @Setting(key = "web.http." + ApiContext.PUBLIC + ".path", description = "Path for " + ApiContext.PUBLIC + " api context", defaultValue = DEFAULT_PUBLIC_PATH)
+            String path
+    ) {
+
+    }
+}
