d2316c3859283805f46d5c4e77b2ee682081c02c
Lars Geyer-Blaumeiser <lars.blaumeiser@cofinity-x.com>
2025-06-30 16:24:30 +0200

    chore: Remove dast and multi-tenancy legacy samples
    Signed-off-by: Lars Geyer-Blaumeiser <lars.blaumeiser@cofinity-x.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -97,11 +97,7 @@ include(":edc-dataplane")
 include(":edc-dataplane:edc-dataplane-base")
 include(":edc-dataplane:edc-dataplane-hashicorp-vault")
 
-
-include(":samples:multi-tenancy")
 include(":samples:testing-with-mocked-connector")
-include(":samples:edc-dast:edc-dast-runtime")
-include(":samples:edc-dast:edc-dast-extensions")
 
 
 // this is needed to have access to snapshot builds of plugins
