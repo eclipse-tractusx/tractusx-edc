4efb9795d97774ea5d6fd24c0f5dc2b0769136cd
farhin23 <farhin.jahan@isst.fraunhofer.de>
2025-07-06 18:18:35 +0200

    Add tests for ActionTypeIs and ArrayIsEmpty validators

--- edc-extensions/cx-policy/src/main/java/org/eclipse/tractusx/edc/policy/cx/validator/ArrayIsEmpty.java
+++ edc-extensions/cx-policy/src/main/java/org/eclipse/tractusx/edc/policy/cx/validator/ArrayIsEmpty.java
@@ -19,17 +19,31 @@ public class ArrayIsEmpty implements Validator<JsonObject> {
 
     @Override
     public ValidationResult validate(JsonObject input) {
+        // Missing attribute - return success
         if (!input.containsKey(arrayAttribute)) {
             return ValidationResult.success();
         }
 
-        var array = input.get(arrayAttribute);
-        if (array.getValueType() != JsonValue.ValueType.ARRAY || !array.asJsonArray().isEmpty()) {
+        var value = input.get(arrayAttribute);
+
+        // Null value - return success
+        if (value.getValueType() == JsonValue.ValueType.NULL) {
+            return ValidationResult.success();
+        }
+
+        // Non-array type - return failure with specific message
+        if (value.getValueType() != JsonValue.ValueType.ARRAY) {
             return ValidationResult.failure(
-                    violation("Access policy must not contain any " + arrayAttribute, path.toString())
+                    violation(arrayAttribute + " must be of type array", path.toString())
             );
         }
 
+        // Array type - must be empty
+        if (!value.asJsonArray().isEmpty()) {
+            return ValidationResult.failure(
+                    violation(arrayAttribute + " must be empty", path.toString())
+            );
+        }
         return ValidationResult.success();
     }
 }
