7f06224df455bc2fb18ca15d39c3b11d54340d03
correiaafonso12 <afonsomfcorreia@gmail.com>
2025-07-08 16:54:04 +0100

    fix: headers

--- edc-extensions/non-finite-provider-push/non-finite-provider-push-core/src/main/java/org/eclipse/tractusx/edc/non/finite/provider/push/core/pipeline/NonFiniteCapablePipelineServiceExtension.java
+++ edc-extensions/non-finite-provider-push/non-finite-provider-push-core/src/main/java/org/eclipse/tractusx/edc/non/finite/provider/push/core/pipeline/NonFiniteCapablePipelineServiceExtension.java
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (c) 2023 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)
+ * Copyright (c) 2025 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
