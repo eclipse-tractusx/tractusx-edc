7bd12f1f31b410059a1ea8fe471a093334406da2
farhin23 <farhin.jahan@isst.fraunhofer.de>
2025-07-09 11:20:10 +0200

    Add tests for UsagePolicyValidator

---
+++ edc-extensions/cx-policy/src/main/java/org/eclipse/tractusx/edc/policy/cx/validator/AtLeastOneRuleExists.java
@@ -0,0 +1,38 @@
+package org.eclipse.tractusx.edc.policy.cx.validator;
+
+import jakarta.json.JsonObject;
+import jakarta.json.JsonValue;
+import org.eclipse.edc.validator.jsonobject.JsonLdPath;
+import org.eclipse.edc.validator.spi.ValidationResult;
+import org.eclipse.edc.validator.spi.Validator;
+
+import static org.eclipse.edc.jsonld.spi.PropertyAndTypeNames.*;
+import static org.eclipse.edc.validator.spi.Violation.violation;
+
+public class AtLeastOneRuleExists implements Validator<JsonObject> {
+    private final JsonLdPath path;
+
+    public AtLeastOneRuleExists(JsonLdPath path) {
+        this.path = path;
+    }
+
+    @Override
+    public ValidationResult validate(JsonObject input) {
+        boolean hasPermission = hasNonEmptyArray(input, ODRL_PERMISSION_ATTRIBUTE);
+        boolean hasObligation = hasNonEmptyArray(input, ODRL_OBLIGATION_ATTRIBUTE);
+        boolean hasProhibition = hasNonEmptyArray(input, ODRL_PROHIBITION_ATTRIBUTE);
+
+        if (hasPermission || hasObligation || hasProhibition) {
+            return ValidationResult.success();
+        }
+        return ValidationResult.failure(
+                violation("Policy must contain at least one permission, obligation, or prohibition", path.toString())
+        );
+    }
+
+    private boolean hasNonEmptyArray(JsonObject input, String attribute) {
+        return input.containsKey(attribute) &&
+                input.get(attribute).getValueType() == JsonValue.ValueType.ARRAY &&
+                !input.get(attribute).asJsonArray().isEmpty();
+    }
+}
