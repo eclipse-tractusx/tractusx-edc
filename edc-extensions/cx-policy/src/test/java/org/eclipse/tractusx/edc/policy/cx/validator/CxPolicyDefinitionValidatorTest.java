14c0e8ee5ef1d1eb985d77c41b8a29263d94302b
farhin23 <farhin.jahan@isst.fraunhofer.de>
2025-07-11 12:45:10 +0200

    Update ArrayIsEmpty tests

--- edc-extensions/cx-policy/src/test/java/org/eclipse/tractusx/edc/policy/cx/validator/CxPolicyDefinitionValidatorTest.java
+++ edc-extensions/cx-policy/src/test/java/org/eclipse/tractusx/edc/policy/cx/validator/CxPolicyDefinitionValidatorTest.java
@@ -8,8 +8,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.eclipse.edc.jsonld.spi.JsonLdKeywords.ID;
 import static org.eclipse.edc.jsonld.spi.JsonLdKeywords.TYPE;
-import static org.eclipse.edc.jsonld.spi.PropertyAndTypeNames.ODRL_PERMISSION_ATTRIBUTE;
-import static org.eclipse.edc.jsonld.spi.PropertyAndTypeNames.ODRL_POLICY_TYPE_SET;
+import static org.eclipse.edc.jsonld.spi.PropertyAndTypeNames.*;
 import static org.eclipse.edc.spi.constants.CoreConstants.EDC_NAMESPACE;
 import static org.eclipse.tractusx.edc.policy.cx.validator.PolicyBuilderFixtures.*;
 import static org.eclipse.tractusx.edc.policy.cx.validator.PolicyValidationConstants.*;
@@ -20,6 +19,19 @@ class CxPolicyDefinitionValidatorTest {
     void shouldReturnSuccess_whenValidAccessPolicy() {
         JsonObject constraint = atomicConstraint(MEMBERSHIP_LITERAL);
         JsonObject permission = rule(ACTION_ACCESS, constraint);
+        JsonObject policy = policy(ODRL_PERMISSION_ATTRIBUTE, permission);
+        JsonObject input = policyDefinition(policy, "some-id");
+
+        ValidationResult result = CxPolicyDefinitionValidator.instance().validate(input);
+
+        assertThat(result.succeeded()).isTrue();
+    }
+
+    @Test
+    void shouldReturnSuccess_whenValidAccessPolicyWithLogicalConstraint() {
+        JsonObject constraint = atomicConstraint(MEMBERSHIP_LITERAL);
+        JsonObject logicalConstraint = logicalConstraint(ODRL_AND_CONSTRAINT_ATTRIBUTE, constraint);
+        JsonObject permission = rule(ACTION_ACCESS, logicalConstraint);
         JsonObject policy = policy(ODRL_PERMISSION_ATTRIBUTE, permission);
         JsonObject input = policyDefinition(policy, "some-id");
 
@@ -89,5 +101,19 @@ class CxPolicyDefinitionValidatorTest {
         ValidationResult result = CxPolicyDefinitionValidator.instance().validate(input);
 
         assertThat(result.succeeded()).isTrue();
+    }
+    @Test
+    void shouldReturnFailure_whenRuleContainDifferentPolicyType() {
+        JsonObject usageConstraint = atomicConstraint(USAGE_PURPOSE_LITERAL);
+        JsonObject usagePermission = rule(ACTION_USAGE, usageConstraint);
+        JsonObject accessConstraint = atomicConstraint(MEMBERSHIP_LITERAL);
+        JsonObject accessPermission = rule(ACTION_ACCESS, accessConstraint);
+        JsonObject policy = policy(ODRL_PERMISSION_ATTRIBUTE, usagePermission, accessPermission);
+        JsonObject input = policyDefinition(policy, "some-id");
+
+        ValidationResult result = CxPolicyDefinitionValidator.instance().validate(input);
+
+        assertThat(result.failed()).isTrue();
+        assertThat(result.getFailureMessages()).anyMatch(msg -> msg.contains("Action was expected to be '%s' but was '%s'".formatted(ACTION_USAGE, ACTION_ACCESS)));
     }
 }
