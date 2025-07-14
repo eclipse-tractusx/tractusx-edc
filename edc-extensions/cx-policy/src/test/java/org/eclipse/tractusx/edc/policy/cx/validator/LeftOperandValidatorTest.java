34623cc79a1463588bda443a831f47e23b69fa5d
farhin23 <farhin.jahan@isst.fraunhofer.de>
2025-07-14 13:51:50 +0200

    Update LeftOperandValidatorTest for different rule type

--- edc-extensions/cx-policy/src/test/java/org/eclipse/tractusx/edc/policy/cx/validator/LeftOperandValidatorTest.java
+++ edc-extensions/cx-policy/src/test/java/org/eclipse/tractusx/edc/policy/cx/validator/LeftOperandValidatorTest.java
@@ -11,32 +11,53 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.eclipse.edc.jsonld.spi.JsonLdKeywords.ID;
-import static org.eclipse.edc.jsonld.spi.PropertyAndTypeNames.ODRL_PERMISSION_ATTRIBUTE;
-import static org.eclipse.tractusx.edc.policy.cx.validator.PolicyValidationConstants.ACCESS_POLICY_TYPE;
-import static org.eclipse.tractusx.edc.policy.cx.validator.PolicyValidationConstants.USAGE_POLICY_TYPE;
+import static org.eclipse.edc.jsonld.spi.PropertyAndTypeNames.*;
+import static org.eclipse.tractusx.edc.policy.cx.validator.PolicyValidationConstants.*;
 
 class LeftOperandValidatorTest {
 
     private final JsonLdPath path = JsonLdPath.path();
 
     @Test
-    void shouldReturnSuccess_whenValidAccessPolicyLeftOperand() {
+    void shouldReturnSuccess_whenValidAccessPermissionPolicyLeftOperand() {
         JsonObject input = Json.createObjectBuilder()
-                .add(ID, "https://w3id.org/catenax/policy/FrameworkAgreement")
+                .add(ID, FRAMEWORK_AGREEMENT_LITERAL)
                 .build();
 
-        ValidationResult result = validateLeftOperand(input, ACCESS_POLICY_TYPE);
+        ValidationResult result = validateLeftOperand(input, ACCESS_POLICY_TYPE, ODRL_PERMISSION_ATTRIBUTE);
 
         assertThat(result.succeeded()).isTrue();
     }
 
     @Test
-    void shouldReturnSuccess_whenValidUsagePolicyLeftOperand() {
+    void shouldReturnSuccess_whenValidUsagePermissionPolicyLeftOperand() {
         JsonObject input = Json.createObjectBuilder()
-                .add(ID, "https://w3id.org/catenax/policy/UsagePurpose")
+                .add(ID, USAGE_PURPOSE_LITERAL)
                 .build();
 
-        ValidationResult result = validateLeftOperand(input, USAGE_POLICY_TYPE);
+        ValidationResult result = validateLeftOperand(input, USAGE_POLICY_TYPE, ODRL_PERMISSION_ATTRIBUTE);
+
+        assertThat(result.succeeded()).isTrue();
+    }
+
+    @Test
+    void shouldReturnSuccess_whenValidUsageProhibitionPolicyLeftOperand() {
+        JsonObject input = Json.createObjectBuilder()
+                .add(ID, USAGE_PURPOSE_LITERAL)
+                .build();
+
+        ValidationResult result = validateLeftOperand(input, USAGE_POLICY_TYPE, ODRL_PROHIBITION_ATTRIBUTE);
+
+        assertThat(result.succeeded()).isTrue();
+    }
+
+    @Test
+    void shouldReturnSuccess_whenValidUsageObligationPolicyLeftOperand() {
+        JsonObject input = Json.createObjectBuilder()
+                .add(ID, AFFILIATES_REGION_LITERAL)
+                .build();
+
+        ValidationResult result = validateLeftOperand(input, USAGE_POLICY_TYPE, ODRL_OBLIGATION_ATTRIBUTE);
 
         assertThat(result.succeeded()).isTrue();
     }
@@ -45,18 +66,18 @@ class LeftOperandValidatorTest {
     void shouldReturnFailure_whenIdIsMissing() {
         JsonObject input = Json.createObjectBuilder().build();
 
-        ValidationResult result = validateLeftOperand(input, ACCESS_POLICY_TYPE);
+        ValidationResult result = validateLeftOperand(input, ACCESS_POLICY_TYPE, ODRL_PERMISSION_ATTRIBUTE);
 
         assertThat(result.failed()).isTrue();
     }
 
     @Test
-    void shouldReturnFailure_whenLeftOperandNotAllowedForAccessPolicy() {
+    void shouldReturnFailure_whenLeftOperandNotAllowedForAccessPermissionPolicy() {
         JsonObject input = Json.createObjectBuilder()
-                .add(ID, "https://w3id.org/catenax/policy/UsagePurpose")
+                .add(ID, USAGE_PURPOSE_LITERAL)
                 .build();
 
-        ValidationResult result = validateLeftOperand(input, ACCESS_POLICY_TYPE);
+        ValidationResult result = validateLeftOperand(input, ACCESS_POLICY_TYPE, ODRL_PERMISSION_ATTRIBUTE);
 
         assertThat(result.failed()).isTrue();
         assertThat(result.getFailureMessages())
@@ -64,12 +85,38 @@ class LeftOperandValidatorTest {
     }
 
     @Test
-    void shouldReturnFailure_whenLeftOperandNotAllowedForUsagePolicy() {
+    void shouldReturnFailure_whenLeftOperandNotAllowedForUsagePermissionPolicy() {
         JsonObject input = Json.createObjectBuilder()
-                .add(ID, "https://w3id.org/catenax/policy/BusinessPartnerGroup")
+                .add(ID, BUSINESS_PARTNER_GROUP_LITERAL)
                 .build();
 
-        ValidationResult result = validateLeftOperand(input, USAGE_POLICY_TYPE);
+        ValidationResult result = validateLeftOperand(input, USAGE_POLICY_TYPE, ODRL_PERMISSION_ATTRIBUTE);
+
+        assertThat(result.failed()).isTrue();
+        assertThat(result.getFailureMessages())
+                .anyMatch(msg -> msg.contains("is not allowed"));
+    }
+
+    @Test
+    void shouldReturnFailure_whenLeftOperandNotAllowedForUsageProhibitionPolicy() {
+        JsonObject input = Json.createObjectBuilder()
+                .add(ID, AFFILIATES_REGION_LITERAL)
+                .build();
+
+        ValidationResult result = validateLeftOperand(input, USAGE_POLICY_TYPE, ODRL_PROHIBITION_ATTRIBUTE);
+
+        assertThat(result.failed()).isTrue();
+        assertThat(result.getFailureMessages())
+                .anyMatch(msg -> msg.contains("is not allowed"));
+    }
+
+    @Test
+    void shouldReturnFailure_whenLeftOperandNotAllowedForUsageObligationPolicy() {
+        JsonObject input = Json.createObjectBuilder()
+                .add(ID, USAGE_PURPOSE_LITERAL)
+                .build();
+
+        ValidationResult result = validateLeftOperand(input, USAGE_POLICY_TYPE, ODRL_OBLIGATION_ATTRIBUTE);
 
         assertThat(result.failed()).isTrue();
         assertThat(result.getFailureMessages())
@@ -82,7 +129,7 @@ class LeftOperandValidatorTest {
                 .add(ID, "invalid-operand")
                 .build();
 
-        ValidationResult result = validateLeftOperand(input, ACCESS_POLICY_TYPE);
+        ValidationResult result = validateLeftOperand(input, ACCESS_POLICY_TYPE, ODRL_PERMISSION_ATTRIBUTE);
 
         assertThat(result.failed()).isTrue();
     }
@@ -94,13 +141,13 @@ class LeftOperandValidatorTest {
                 .add(ID, id)
                 .build();
 
-        ValidationResult result = validateLeftOperand(input, ACCESS_POLICY_TYPE);
+        ValidationResult result = validateLeftOperand(input, ACCESS_POLICY_TYPE, ODRL_PERMISSION_ATTRIBUTE);
 
         assertThat(result.failed()).isTrue();
     }
 
-    private ValidationResult validateLeftOperand(JsonObject input, String policyType) {
-        return LeftOperandValidator.instance(JsonObjectValidator.newValidator(), policyType, ODRL_PERMISSION_ATTRIBUTE)
+    private ValidationResult validateLeftOperand(JsonObject input, String policyType, String ruleType) {
+        return LeftOperandValidator.instance(JsonObjectValidator.newValidator(), policyType, ruleType)
                 .build()
                 .validate(input);
     }
