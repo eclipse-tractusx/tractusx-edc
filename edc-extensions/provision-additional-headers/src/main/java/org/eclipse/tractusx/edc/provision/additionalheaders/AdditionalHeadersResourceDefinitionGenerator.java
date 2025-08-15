54c121dd53c6c702b8a28e37389193d99d06a6b4
Ronja Quensel <ronja.quensel@isst.fraunhofer.de>
2025-08-15 16:04:58 +0200

    chore: include BDRS into additional headers provider

--- edc-extensions/provision-additional-headers/src/main/java/org/eclipse/tractusx/edc/provision/additionalheaders/AdditionalHeadersResourceDefinitionGenerator.java
+++ edc-extensions/provision-additional-headers/src/main/java/org/eclipse/tractusx/edc/provision/additionalheaders/AdditionalHeadersResourceDefinitionGenerator.java
@@ -27,31 +27,40 @@ import org.eclipse.edc.connector.controlplane.transfer.spi.types.ResourceDefinit
 import org.eclipse.edc.connector.controlplane.transfer.spi.types.TransferProcess;
 import org.eclipse.edc.policy.model.Policy;
 import org.eclipse.edc.spi.types.domain.DataAddress;
+import org.eclipse.tractusx.edc.spi.identity.mapper.BdrsClient;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Optional;
 import java.util.UUID;
 
+import static org.eclipse.tractusx.edc.spi.identity.mapper.BdrsConstants.DID_PREFIX;
+
 class AdditionalHeadersResourceDefinitionGenerator implements ProviderResourceDefinitionGenerator {
 
     private final ContractAgreementService contractAgreementService;
+    private final BdrsClient bdrsClient;
 
-    AdditionalHeadersResourceDefinitionGenerator(ContractAgreementService contractAgreementService) {
+    AdditionalHeadersResourceDefinitionGenerator(ContractAgreementService contractAgreementService, BdrsClient bdrsClient) {
         this.contractAgreementService = contractAgreementService;
+        this.bdrsClient = bdrsClient;
     }
 
     @Override
     public @Nullable ResourceDefinition generate(TransferProcess transferProcess, DataAddress dataAddress, Policy policy) {
-        var bpn = Optional.of(transferProcess.getContractId())
+        var identity = Optional.of(transferProcess.getContractId())
                 .map(contractAgreementService::findById)
                 .map(ContractAgreement::getConsumerId)
                 .orElse(null);
-
+        
+        if (identity != null && identity.startsWith(DID_PREFIX)) {
+            identity = bdrsClient.resolveBpn(identity);
+        }
+        
         return AdditionalHeadersResourceDefinition.Builder.newInstance()
                 .id(UUID.randomUUID().toString())
                 .dataAddress(dataAddress)
                 .contractId(transferProcess.getContractId())
-                .bpn(bpn)
+                .bpn(identity)
                 .build();
     }
 
