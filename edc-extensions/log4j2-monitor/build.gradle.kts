e15a6d573b12e88f636deb098c469508396a7ab5
Karol Kotowicz <kkotowiczz@gmail.com>
2025-08-27 11:41:48 +0200

    feat(observability): Log4j2 integration with OTEL
    Added dependency which allows otel appender to be utilised in umbrella helmcharts

--- edc-extensions/log4j2-monitor/build.gradle.kts
+++ edc-extensions/log4j2-monitor/build.gradle.kts
@@ -10,7 +10,7 @@ dependencies {
     implementation(libs.opentelemetry.instrumentation.annotations)
     testImplementation(libs.edc.junit)
     testImplementation(libs.log4j2.core.test)
-    implementation("io.opentelemetry.instrumentation:opentelemetry-log4j-appender-2.17:1.32-alpha")
+    runtimeOnly("io.opentelemetry.instrumentation:opentelemetry-log4j-appender-2.17:1.32.0-alpha")
 }
 
 
