e272588e31642ef60979d8e3a35e155c0467f1ac
Lars Geyer-Blaumeiser <lars.blaumeiser@cofinity-x.com>
2025-10-14 13:17:14 +0200

    Add copyright notice
    Signed-off-by: Lars Geyer-Blaumeiser <lars.blaumeiser@cofinity-x.com>

--- edc-tests/e2e/edc-dataplane-tokenrefresh-tests/src/test/java/org/eclipse/tractusx/edc/dataplane/tokenrefresh/e2e/DataPlaneTokenRefreshEndToEndTest.java
+++ edc-tests/e2e/edc-dataplane-tokenrefresh-tests/src/test/java/org/eclipse/tractusx/edc/dataplane/tokenrefresh/e2e/DataPlaneTokenRefreshEndToEndTest.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2024 Bayerische Motoren Werke Aktiengesellschaft
+ * Copyright (c) 2025 Cofinity-X GmbH
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
