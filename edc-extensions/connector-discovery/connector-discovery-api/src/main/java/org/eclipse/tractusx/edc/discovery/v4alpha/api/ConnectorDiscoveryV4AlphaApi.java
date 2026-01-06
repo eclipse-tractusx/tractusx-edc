ecc322df3967909719fd536676dd3e237007ca4e
Lars Geyer-Blaumeiser <lars.blaumeiser@cofinity-x.com>
2026-01-06 17:59:52 +0100

    Add API description of service discovery endpoint
    Signed-off-by: Lars Geyer-Blaumeiser <lars.blaumeiser@cofinity-x.com>

--- edc-extensions/connector-discovery/connector-discovery-api/src/main/java/org/eclipse/tractusx/edc/discovery/v4alpha/api/ConnectorDiscoveryV4AlphaApi.java
+++ edc-extensions/connector-discovery/connector-discovery-api/src/main/java/org/eclipse/tractusx/edc/discovery/v4alpha/api/ConnectorDiscoveryV4AlphaApi.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2025 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)
+ * Copyright (c) 2026 Cofinity-X GmbH
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
@@ -32,10 +33,11 @@ import jakarta.json.JsonObject;
 import org.eclipse.edc.web.spi.ApiErrorDetail;
 import org.eclipse.tractusx.edc.discovery.v4alpha.spi.ConnectorParamsDiscoveryRequest;
 
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.NOT_REQUIRED;
 import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 import static org.eclipse.edc.jsonld.spi.JsonLdKeywords.CONTEXT;
 import static org.eclipse.edc.jsonld.spi.JsonLdKeywords.TYPE;
-
+import static org.eclipse.tractusx.edc.edr.spi.CoreConstants.TX_NAMESPACE;
 
 @OpenAPIDefinition(info = @Info(description = "With this API clients discover EDC requesting parameters according to different DSP versions", title = "Connector Discovery API"))
 @Tag(name = "Connector Discovery")
@@ -55,6 +57,21 @@ public interface ConnectorDiscoveryV4AlphaApi {
             })
     JsonObject discoverDspVersionParamsV4Alpha(JsonObject querySpecJson);
 
+    @Operation(description = "Retrieves 'DataService' Entries from the DID document of a participant and provides the connection parameters for all found",
+            requestBody = @RequestBody(content = @Content(schema = @Schema(name = "Service Discovery Request", implementation = ServiceDiscoveryRequestSchema.class))),
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "A list of connector endpoint parameters for the right version for each found connector",
+                            content = @Content(array = @ArraySchema(schema = @Schema(name = "Service Discovery Response", implementation = ConnectorDiscoveryResponse.class)))),
+                    @ApiResponse(responseCode = "400", description = "Request body was malformed",
+                            content = @Content(array = @ArraySchema(schema = @Schema(implementation = ApiErrorDetail.class)))),
+                    @ApiResponse(responseCode = "404", description = "Given identifier could not be resolved",
+                            content = @Content(array = @ArraySchema(schema = @Schema(implementation = ApiErrorDetail.class)))),
+                    @ApiResponse(responseCode = "500", description = "Discovery failed due to an internal error",
+                            content = @Content(array = @ArraySchema(schema = @Schema(implementation = ApiErrorDetail.class)))),
+                    @ApiResponse(responseCode = "502", description = "Discovery failed due to connection to counter party",
+                            content = @Content(array = @ArraySchema(schema = @Schema(implementation = ApiErrorDetail.class))))
+            })
+    JsonObject discoverConnectorServicesV4Alpha(JsonObject querySpecJson);
 
     @Schema(name = "ConnectorParamsDiscoveryRequest", example = ConnectorParamsDiscoveryRequestSchema.EXAMPLE)
     record ConnectorParamsDiscoveryRequestSchema(
@@ -80,7 +97,6 @@ public interface ConnectorDiscoveryV4AlphaApi {
                 """;
     }
 
-
     @Schema(name = "ConnectorDiscoveryResponse", example = ConnectorDiscoveryResponse.EXAMPLE)
     record ConnectorDiscoveryResponse(
             Object context,
@@ -102,4 +118,33 @@ public interface ConnectorDiscoveryV4AlphaApi {
                 """;
     }
 
+    @Schema(name = "ServiceDiscoveryRequestSchema", example = ServiceDiscoveryRequestSchema.EXAMPLE)
+    record ServiceDiscoveryRequestSchema(
+            @Schema(name = CONTEXT, requiredMode = REQUIRED)
+            Object context,
+            @Schema(name = TYPE, example = CSDR_TYPE)
+            String type,
+            @Schema(requiredMode = REQUIRED)
+            String identifier,
+            @Schema(requiredMode = NOT_REQUIRED)
+            String[] knowns
+    ) {
+        public static final String CSDR_TYPE = TX_NAMESPACE +  "ConnectorServiceDiscoveryRequest";
+        public static final String SERVICE_DISCOVERY_REQUEST_IDENTIFIER_ATTRIBUTE = TX_NAMESPACE + "identifier";
+        public static final String SERVICE_DISCOVERY_REQUEST_KNOWNS_ATTRIBUTE = TX_NAMESPACE + "knowns";
+
+        public static final String EXAMPLE = """
+                    {
+                        "@context": {
+                            "tx": "https://w3id.org/tractusx/v0.0.1/ns/"
+                        },
+                        "@type": "tx:ConnectorParamsDiscoveryRequest",
+                        "tx:identifier": "did:web:one-example.com",
+                        "tx:knowns": [
+                            "https://provider.domain.com/conn1/api/dsp",
+                            "https://provider.domain.com/conn2/api/v1/dsp",
+                        ]
+                    }
+                    """;
+    }
 }
