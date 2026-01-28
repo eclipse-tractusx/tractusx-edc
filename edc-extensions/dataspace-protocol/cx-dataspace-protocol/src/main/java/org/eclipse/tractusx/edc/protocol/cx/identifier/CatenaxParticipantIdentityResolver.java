1dbce865a2841a69fa3b2b82d063438a71e7bc19
pratapipatelbcone <pratapi.hemant.patel@sap.com>
2026-01-28 20:32:32 +0530

    feat: Remove unused dependency from libs.version.toml

--- edc-extensions/dataspace-protocol/cx-dataspace-protocol/src/main/java/org/eclipse/tractusx/edc/protocol/cx/identifier/CatenaxParticipantIdentityResolver.java
+++ edc-extensions/dataspace-protocol/cx-dataspace-protocol/src/main/java/org/eclipse/tractusx/edc/protocol/cx/identifier/CatenaxParticipantIdentityResolver.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2025 Think-it GmbH
+ * Copyright (c) 2026 SAP SE
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
