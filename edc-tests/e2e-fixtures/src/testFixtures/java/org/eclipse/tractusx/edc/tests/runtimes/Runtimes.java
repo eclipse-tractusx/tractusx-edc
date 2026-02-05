6f33338f8efebe0744aa4cd104f8bdb3205edaa8
Bruno Nogueira <7brunon@gmail.com>
2026-02-05 09:11:32 +0000

    fix: copyright years in Copyright

--- edc-tests/e2e-fixtures/src/testFixtures/java/org/eclipse/tractusx/edc/tests/runtimes/Runtimes.java
+++ edc-tests/e2e-fixtures/src/testFixtures/java/org/eclipse/tractusx/edc/tests/runtimes/Runtimes.java
@@ -1,6 +1,6 @@
 /********************************************************************************
  * Copyright (c) 2025 Cofinity-X GmbH
- * Copyright (c) 2026 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)
+ * Copyright (c) 2024 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
