049b22832f6705f2cb49a51302101530630e7631
Lars Geyer-Blaumeiser <lars.blaumeiser@cofinity-x.com>
2026-04-14 17:44:06 +0200

    Fix checkstyle issues
    Signed-off-by: Lars Geyer-Blaumeiser <lars.blaumeiser@cofinity-x.com>

--- edc-extensions/did-document/did-document-service-self-registration/src/test/java/org/eclipse/tractusx/edc/did/document/service/self/registration/DidDocumentServiceSelfRegistrationExtensionTest.java
+++ edc-extensions/did-document/did-document-service-self-registration/src/test/java/org/eclipse/tractusx/edc/did/document/service/self/registration/DidDocumentServiceSelfRegistrationExtensionTest.java
@@ -33,9 +33,6 @@ import org.eclipse.tractusx.edc.spi.did.document.service.DidDocumentServiceClien
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EmptySource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Map;
 
