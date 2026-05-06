d27f669e19d312e466abc297c69c4f9c9470f34b
MDSBarbosa <CTW02247@criticaltechworks.com>
2026-05-06 11:51:23 +0100

    feat: remove validation from UsagePurposeConstraintFunction

--- edc-extensions/cx-policy/src/test/java/org/eclipse/tractusx/edc/policy/cx/usage/UsagePurposeConstraintFunctionTest.java
+++ edc-extensions/cx-policy/src/test/java/org/eclipse/tractusx/edc/policy/cx/usage/UsagePurposeConstraintFunctionTest.java
@@ -19,18 +19,15 @@
 
 package org.eclipse.tractusx.edc.policy.cx.usage;
 
-import jakarta.json.Json;
 import org.eclipse.edc.participant.spi.ParticipantAgent;
 import org.eclipse.edc.participant.spi.ParticipantAgentPolicyContext;
 import org.eclipse.edc.policy.model.Operator;
 import org.eclipse.tractusx.edc.policy.cx.TestParticipantAgentPolicyContext;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
-import java.util.Map;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.eclipse.edc.junit.assertions.AbstractResultAssert.assertThat;
 import static org.mockito.Mockito.mock;
 
 class UsagePurposeConstraintFunctionTest {
@@ -40,28 +37,13 @@ class UsagePurposeConstraintFunctionTest {
     private final ParticipantAgentPolicyContext context = new TestParticipantAgentPolicyContext(participantAgent);
 
     @Test
-    void evaluate() {
-        assertThat(function.evaluate(Operator.IS_ANY_OF, List.of("cx.core.legalRequirementForThirdparty:1", "cx.core.industrycore:1"), null, context)).isTrue();
+    void evaluate_shouldReturnTrue_withAnyRightValue() {
+        assertThat(function.evaluate(Operator.IS_ANY_OF, "anyValue", null, context)).isTrue();
     }
 
-    @Test
-    void validate_whenOperatorAndRightOperandAreValid_thenSuccess() {
-        var legalRequirementForThirdparty = Json.createValue("cx.core.legalRequirementForThirdparty:1");
-        var industrycore = Json.createValue("cx.core.industrycore:1");
-        var rightValue = List.of(Map.of("@value", legalRequirementForThirdparty), Map.of("@value", industrycore));
-        var result = function.validate(Operator.IS_ANY_OF, rightValue, null);
-        assertThat(result).isSucceeded();
-    }
-
-    @Test
-    void validate_whenInvalidOperator_thenFailure() {
-        var result = function.validate(Operator.EQ, List.of("cx.core.legalRequirementForThirdparty:1", "cx.core.industrycore:1"), null);
-        assertThat(result).isFailed().detail().contains("Invalid operator");
-    }
-
-    @Test
-    void validate_whenInvalidValue_thenFailure() {
-        var result = function.validate(Operator.IS_ANY_OF, List.of("BPNL00000000001A"), null);
-        assertThat(result).isFailed().detail().contains("Invalid right-operand: ");
+    @ParameterizedTest
+    @EnumSource(Operator.class)
+    void evaluate_shouldReturnTrue_forAllOperators(Operator operator) {
+        assertThat(function.evaluate(operator, "someValue", null, context)).isTrue();
     }
 }
