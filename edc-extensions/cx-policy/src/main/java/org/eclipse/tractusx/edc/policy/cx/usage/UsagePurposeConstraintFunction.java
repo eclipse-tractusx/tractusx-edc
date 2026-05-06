c00174345089d10beb02d7a03da8a5d2296d18f1
Mário Barbosa <107394742+MDSBarbosa@users.noreply.github.com>
2026-05-06 16:52:45 +0100

    feat: remove validation from UsagePurposeConstraintFunction (#2798)
    * feat: remove validation from UsagePurposeConstraintFunction
    
    * feat: ensure only isAnyOf operator is valid for UsagePurposeConstraint

--- edc-extensions/cx-policy/src/main/java/org/eclipse/tractusx/edc/policy/cx/usage/UsagePurposeConstraintFunction.java
+++ edc-extensions/cx-policy/src/main/java/org/eclipse/tractusx/edc/policy/cx/usage/UsagePurposeConstraintFunction.java
@@ -22,7 +22,8 @@ package org.eclipse.tractusx.edc.policy.cx.usage;
 import org.eclipse.edc.participant.spi.ParticipantAgentPolicyContext;
 import org.eclipse.edc.policy.model.Operator;
 import org.eclipse.edc.policy.model.Permission;
-import org.eclipse.tractusx.edc.policy.cx.common.ValueValidatingConstraintFunction;
+import org.eclipse.edc.spi.result.Result;
+import org.eclipse.tractusx.edc.policy.cx.common.BaseConstraintFunction;
 
 import java.util.Set;
 
@@ -30,44 +31,17 @@ import java.util.Set;
  * This is a placeholder constraint function for UsagePurpose. It always returns true but allows
  * the validation of policies to be strictly enforced.
  */
-public class UsagePurposeConstraintFunction<C extends ParticipantAgentPolicyContext> extends ValueValidatingConstraintFunction<Permission, C> {
+public class UsagePurposeConstraintFunction<C extends ParticipantAgentPolicyContext> extends BaseConstraintFunction<Permission, C> {
     public static final String USAGE_PURPOSE = "UsagePurpose";
 
     public UsagePurposeConstraintFunction() {
         super(
-                Set.of(Operator.IS_ANY_OF),
-                Set.of(
-                        "cx.core.legalRequirementForThirdparty:1",
-                        "cx.core.industrycore:1",
-                        "cx.core.qualityNotifications:1",
-                        "cx.core.digitalTwinRegistry:1",
-                        "cx.pcf.base:1",
-                        "cx.quality.base:1",
-                        "cx.dcm.base:1",
-                        "cx.puris.base:1",
-                        "cx.circular.dpp:1",
-                        "cx.circular.smc:1",
-                        "cx.circular.marketplace:1",
-                        "cx.circular.materialaccounting:1",
-                        "cx.bpdm.gate.upload:1",
-                        "cx.bpdm.gate.download:1",
-                        "cx.bpdm.pool:1",
-                        "cx.bpdm.vas.countryrisk:1",
-                        "cx.bpdm.vas.dataquality.upload:1",
-                        "cx.bpdm.vas.dataquality.download:1",
-                        "cx.bpdm.vas.bdv.upload:1",
-                        "cx.bpdm.vas.bdv.download:1",
-                        "cx.bpdm.vas.fpd.upload:1",
-                        "cx.bpdm.vas.fpd.download:1",
-                        "cx.bpdm.vas.swd.upload:1",
-                        "cx.bpdm.vas.swd.download:1",
-                        "cx.bpdm.vas.nps.upload:1",
-                        "cx.bpdm.vas.nps.download:1",
-                        "cx.ccm.base:1",
-                        "cx.bpdm.poolAll:1",
-                        "cx.logistics.base:1"
-                ),
-                true
+                Set.of(Operator.IS_ANY_OF)
         );
+    }
+
+    @Override
+    protected Result<Void> validateRightOperand(Object rightValue) {
+        return Result.success();
     }
 }
