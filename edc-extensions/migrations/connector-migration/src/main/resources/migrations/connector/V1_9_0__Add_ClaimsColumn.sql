dc31edcac100cdc593c53666a7e3ba3075312992
Lars Geyer-Blaumeiser <lars.blaumeiser@cofinity-x.com>
2026-06-03 10:11:57 +0200

    Add missing migration
    Signed-off-by: Lars Geyer-Blaumeiser <lars.blaumeiser@cofinity-x.com>

--- edc-extensions/migrations/connector-migration/src/main/resources/migrations/connector/V1_9_0__Add_ClaimsColumn.sql
+++ edc-extensions/migrations/connector-migration/src/main/resources/migrations/connector/V1_9_0__Add_ClaimsColumn.sql
@@ -1,1 +1,19 @@
-ALTER TABLE edc_contract_agreement ADD COLUMN claims JSON;
+--
+--  Copyright (c) 2026 Cofinity-X GmbH
+--
+--  This program and the accompanying materials are made available under the
+--  terms of the Apache License, Version 2.0 which is available at
+--  https://www.apache.org/licenses/LICENSE-2.0
+--
+--  SPDX-License-Identifier: Apache-2.0
+--
+--  Contributors:
+--       Cofinity-X GmbH - initial API and implementation
+--
+--
+-- table: edc_contract_agreement, edc_transfer_process
+--
+
+ALTER TABLE edc_contract_agreement ADD COLUMN IF NOT EXISTS claims JSON;
+
+ALTER TABLE edc_transfer_process ADD COLUMN IF NOT EXISTS claims JSON;
