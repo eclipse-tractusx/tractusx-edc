b7a66c4e2b1ba8e0a622197020a759d8a8bd74f3
Lars Geyer-Blaumeiser <lars.blaumeiser@cofinity-x.com>
2026-06-03 14:52:13 +0200

    Fix participant context id issue for dataplane
    Signed-off-by: Lars Geyer-Blaumeiser <lars.blaumeiser@cofinity-x.com>

--- edc-dataplane/edc-dataplane-base/build.gradle.kts
+++ edc-dataplane/edc-dataplane-base/build.gradle.kts
@@ -53,9 +53,8 @@ dependencies {
     runtimeOnly(libs.edc.aws.validator.data.address.s3)
     runtimeOnly(libs.edc.core.did) // for the DID Public Key Resolver
     runtimeOnly(libs.edc.core.edrstore)
-    runtimeOnly(libs.edc.core.participant.context.config)
+    runtimeOnly(libs.edc.core.participant.context.core)
     runtimeOnly(libs.edc.core.participant.context.classic)
-    runtimeOnly(libs.edc.spi.participant.context)
     runtimeOnly(libs.edc.dpf.awss3)
     runtimeOnly(libs.edc.aws.provision.s3)
     runtimeOnly(libs.edc.dpf.azblob)
