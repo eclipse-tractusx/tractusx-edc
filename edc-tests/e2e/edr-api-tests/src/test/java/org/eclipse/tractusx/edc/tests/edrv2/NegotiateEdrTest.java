8e1d620a271fb9174b226081d983f0165e926b28
bmg13 <gabcmonteiro@gmail.com>
2026-08-20 17:40:08 +0100

    Fix NegotiateEdrTest Tests.

--- edc-tests/e2e/edr-api-tests/src/test/java/org/eclipse/tractusx/edc/tests/edrv2/NegotiateEdrTest.java
+++ edc-tests/e2e/edr-api-tests/src/test/java/org/eclipse/tractusx/edc/tests/edrv2/NegotiateEdrTest.java
@@ -69,6 +69,7 @@ import static org.eclipse.tractusx.edc.tests.TestRuntimeConfiguration.PROVIDER_B
 import static org.eclipse.tractusx.edc.tests.TestRuntimeConfiguration.PROVIDER_DID;
 import static org.eclipse.tractusx.edc.tests.TestRuntimeConfiguration.PROVIDER_NAME;
 import static org.eclipse.tractusx.edc.tests.helpers.EdrNegotiationHelperFunctions.createEvent;
+import static org.eclipse.tractusx.edc.tests.helpers.PolicyHelperFunctions.bpnGroupPolicy;
 import static org.eclipse.tractusx.edc.tests.participant.TractusxParticipantBase.ASYNC_POLL_INTERVAL;
 import static org.eclipse.tractusx.edc.tests.participant.TractusxParticipantBase.ASYNC_TIMEOUT;
 import static org.eclipse.tractusx.edc.tests.runtimes.Runtimes.pgRuntime;
@@ -142,7 +143,7 @@ public class NegotiateEdrTest {
         PROVIDER.createAsset(assetId, Map.of(), dataAddress);
 
         PROVIDER.storeBusinessPartner(CONSUMER.getBpn(), "test-group1", "test-group2");
-        var accessPolicy = PROVIDER.createPolicyDefinition(PolicyHelperFunctions.bpnGroupPolicy(Operator.IS_NONE_OF.name(), true, "forbidden-policy"));
+        var accessPolicy = PROVIDER.createPolicyDefinition(bpnGroupPolicy("isNoneOf", true, "forbidden-policy"));
         var contractPolicy = PROVIDER.createPolicyDefinition(PolicyHelperFunctions.frameworkPolicy(Map.of(), "use"));
         PROVIDER.createContractDefinition(assetId, "def-1", accessPolicy, contractPolicy);
 
